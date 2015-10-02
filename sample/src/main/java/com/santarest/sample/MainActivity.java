e2bed6997640274514beedc3dddec023db1dd919
Vlad Sumtsov <vlad.sumtsov@gmail.com>
2015-10-02 12:38:26 +0300

    add chack for multipart

--- sample/src/main/java/com/santarest/sample/MainActivity.java
+++ sample/src/main/java/com/santarest/sample/MainActivity.java
@@ -2,8 +2,6 @@ package com.santarest.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.santarest.RequestBuilder;
 import com.santarest.SantaRest;
@@ -13,13 +11,14 @@ import com.squareup.otto.Subscribe;
 
 public class MainActivity extends ActionBarActivity {
 
-    private SantaRest santaRest;
+    private SantaRest githubRest;
+    private SantaRest uploadFileServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.santarest.sample.R.layout.activity_main);
-        santaRest = new SantaRest.Builder()
+        githubRest = new SantaRest.Builder()
                 .setServerUrl("https://api.github.com")
                 .setRequestInterceptor(new SantaRest.RequestInterceptor() {
                     @Override
@@ -36,7 +35,40 @@ public class MainActivity extends ActionBarActivity {
 
                 })
                 .build();
+        uploadFileServer = new SantaRest.Builder()
+                .setServerUrl("http://posttestserver.com")
+                .setRequestInterceptor(new SantaRest.RequestInterceptor() {
+                    @Override
+                    public void intercept(RequestBuilder request) {
+                        request.addHeader("test", "test");
+                    }
+                })
+                .addResponseInterceptors(new SantaRest.ResponseListener() {
+                    @Override
+                    public void onResponseReceived(Object action, Request request, Response response) {
+                        System.out.println(request);
+                        System.out.println(response);
+                    }
 
+                })
+                .build();
+
+        uploadFileServer.sendAction(new UploadFileAction());
+        githubRest.sendAction(new ExampleAction("square", "retrofit"));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        githubRest.subscribe(this);
+        uploadFileServer.subscribe(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        githubRest.unsubscribe(this);
+        uploadFileServer.unsubscribe(this);
     }
 
     @Subscribe
@@ -46,38 +78,10 @@ public class MainActivity extends ActionBarActivity {
         System.out.println(action.isSuccess());
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        santaRest.subscribe(this);
-        santaRest.runAction(new ExampleAction("square", "retrofit"));
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        santaRest.unsubscribe(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @Subscribe
+    public void onUploadFileAction(UploadFileAction action) {
+        System.out.println(action);
+        System.out.println(action.success);
+        System.out.println("response = " + action.getResponse());
     }
 }
